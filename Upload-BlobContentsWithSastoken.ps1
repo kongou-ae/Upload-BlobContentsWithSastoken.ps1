b65fb96912032cd38e46a22b1a51e6b22eb8dbab
kongou-ae <y.matsumoto.ae@gmail.com>
2018-06-15 00:08:04 +0900

    Update Upload-BlobContentsWithSastoken.ps1

--- Upload-BlobContentsWithSastoken.ps1
+++ Upload-BlobContentsWithSastoken.ps1
@@ -12,19 +12,15 @@
 	- Upload files to temporally container
 	- Show you the url to access files.
 
-.PARAMETER storageAccountName
-	The storage account name to upload your files.
-
 .PARAMETER targetDir
 	The absolute path where the file you want to upload is stored.
 
 .EXAMPLE
-	.\Upload-BlobContentsWithSastoken.ps1 -storageAccountName yourstorageaccount -targetDir C:\Users\yourname\Documents\20180519
+	.\Upload-BlobContentsWithSastoken.ps1 -targetDir C:\Users\yourname\Documents\20180519
 
 #>
 
 Param(
-  [parameter(mandatory = $true)][String]$storageAccountName,
   [parameter(mandatory = $true)][String]$targetDir
 )
 
@@ -35,8 +31,8 @@ if (!(Get-Module -ListAvailable -Name AzureRM)) {
 
 # Check module. If module don't exist, import module.
 if (!(Get-Module -Name AzureRM)) {
-  Write-Host "AzureRM isn't imported. Start importing AzureRM"
-  Import-Module -Name AzureRM
+  Write-Host "AzureRM isn't installed. Start importing AzureRM"
+  Import-Module -Name AzureRM -MinimumVersion 5.0.0 -Scope Local
 }
 
 # Login if you don't login.
@@ -46,17 +42,17 @@ if (!($content.Account)) {
   Login-AzureRmAccount
 }
 
-# Connect storage account.
-$ctx = (Get-AzureRmStorageAccount | Where-Object {$_.StorageAccountName -eq $storageAccountName }).Context
+$storageAccount = Get-AzureRmStorageAccount | Select-Object ResourceGroupName,StorageAccountName,AccountType | Out-GridView -PassThru 
+$ctx = (Get-AzureRmStorageAccount -ResourceGroupName $storageAccount.ResourceGroupName -Name $storageAccount.StorageAccountName ).Context
 $containerName = (Get-date).ToString("yyyy-MMdd-HHmm")
 
 # Create container and SAS token to share files.
 $null = New-AzureStorageContainer -Name $containerName -Context $ctx -Permission Off
-$sasToken = New-AzureStorageContainerSASToken -Container $containerName -Protocol HttpsOnly -Permission r -ExpiryTime (Get-Date).AddDays(3) -Context $ctx
+$sasToken = New-AzureStorageContainerSASToken -Container $containerName -Protocol HttpsOnly -Permission r -ExpiryTime (Get-Date).AddDays(3) -Context $ctx 
 
 # Upload all files which is in target dir.
 Get-ChildItem $targetDir | ForEach-Object {
-  $null = Set-AzureStorageBlobContent -File $_.FullName -Container $containerName -Context $ctx -Force
+  $null = Set-AzureStorageBlobContent -File $_.FullName -Container $containerName -Context $ctx -Force 
 }
 
 # show the url of file which was uoloaded.
